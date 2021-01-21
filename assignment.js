d454217ae4ad1edfa4996be98b1e5117a93c8f72
Zobayer Akib <akibanon01@gmail.com>
2021-01-22 02:05:48 +0600

    Final commit

--- assignment.js
+++ assignment.js
@@ -1,5 +1,3 @@
-
-
 function kilometerToMeter(kilometer) {
 
     //Convert kilometer to meter(Calculation)
@@ -12,18 +10,18 @@ function kilometerToMeter(kilometer) {
     }    
 }
 
-//var result=kilometerToMeter(10);
-//console.log(result); 
 
-function budgetCalculator(watchNum,phoneNum,laptopNum) {
+
+
+function budgetCalculator(numOfWatch,numOfPhone,numOflaptop) {
     var watchPrice=50;
     var phonePrice=100;
     var laptopPrice=500;
 
     //Calculation of  budget.
-    if(watchNum>0 && phoneNum>0 && laptopNum>0)
+    if(numOfWatch>0 && numOfPhone>0 && numOflaptop>0)
     {
-        var TotalCost=((watchNum*watchPrice)+(phoneNum*phonePrice)+(laptopNum*laptopPrice));
+        var TotalCost=((numOfWatch*watchPrice)+(numOfPhone*phonePrice)+(numOflaptop*laptopPrice));
         return TotalCost;
     }
     else{
@@ -31,8 +29,7 @@ function budgetCalculator(watchNum,phoneNum,laptopNum) {
     }
 }
 
-//var result1=budgetCalculator(2,1,0);
-//console.log(result1);
+
 
 function hotelCost(numberOfDay) {
 
@@ -41,17 +38,28 @@ function hotelCost(numberOfDay) {
     {
         if(numberOfDay>0 && numberOfDay<=10)
         {
+            //For 1st 10 days hotel cost is 100 
+
             var totalCost = (numberOfDay*100);
             return totalCost;
+
         }
 
         else if (numberOfDay>10 && numberOfDay<=20)
-        {
+        {   
+            //For 1st 10 days hotel cost is 100 
+            //For 11th day to 20th day hotel cost is 80
+
             var totalCost = ( (10*100) + ((numberOfDay-10)*80) );
             return totalCost;
+
         }
         else
-        {
+        {   
+            //For 1st 10 days hotel cost is 100 
+            //For 11th day to 20th day hotel cost is 80
+            //For 21th day to nth day hotel cost is 50
+
             var totalCost = ( (10*100) + (10*80) + ((numberOfDay-20)*50) );
             return totalCost;
         }
@@ -62,20 +70,24 @@ function hotelCost(numberOfDay) {
         console.log("Error! Enter the right information ");
     }
 }
-//var result = hotelCost(20);
-//console.log(result);
 
 
 
 
 function megaFriend(arr) {
+
+ //initialize a variable to store the longest word   
  var MegaFriend = "";
+
   for (var i = 0; i < arr.length; i++) {
+
     if (MegaFriend.length < arr[i].length) {
+
         MegaFriend = arr[i];
+
     }
   }
   return MegaFriend;
 }
-var arr=['FaysalMahmud', 'akib', 'mahin']
-console.log(megaFriend(arr));
+
+var arr=['Karim', 'Rahimuddin', 'Takrim'] //storing some name as string in arr variable
